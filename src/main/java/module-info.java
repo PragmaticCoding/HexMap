37f7be27a93930b3bbbf5153c2dd4ba9124951bf
Dave Barrett <barrettda@gmail.com>
2022-07-09 16:22:26 -0400

    Added the missing tank image files.  Problem reported by Brad.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,6 @@
 module ca.pragmaticcoding.hexmap {
     requires javafx.controls;
 
-
     opens ca.pragmaticcoding.hexmap to javafx.controls;
     exports ca.pragmaticcoding.hexmap;
     exports ca.pragmaticcoding.hexmap.tile;
